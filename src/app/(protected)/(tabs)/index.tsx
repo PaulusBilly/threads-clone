8831e11378c9158f365798f5a0ed9b4869480164
Billy <141384133+PaulusBilly@users.noreply.github.com>
2025-05-30 01:50:48 +0700

    added new post page

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { posts } from "@/dummyData";
+import { Link } from "expo-router";
 import PostListItem from "@/components/postListItem";
 
 export default function HomeScreen() {
@@ -7,6 +8,11 @@ export default function HomeScreen() {
     <FlatList
       data={posts}
       renderItem={({ item }) => <PostListItem post={item} />}
+      ListHeaderComponent={() => (
+        <Link href="/new" className="text-blue-500 p-4 text-center text-3xl">
+          New Post
+        </Link>
+      )}
     />
   );
 }
