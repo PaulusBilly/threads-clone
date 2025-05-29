12930e6829fa402f5ad9d481af77f7b70507f08f
Billy <141384133+PaulusBilly@users.noreply.github.com>
2025-05-29 19:24:55 +0700

    added footer icons

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,47 @@
+import { Tabs } from "expo-router";
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function tabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: false }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "Notifications",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
